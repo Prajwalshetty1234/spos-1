85057dc52ffa88e5aabbb7aa0fd36b7b72967a68
Prajwalshetty1234 <107292851+Prajwalshetty1234@users.noreply.github.com>
2022-09-10 11:29:36 +0530

    Update and rename Pratical_2 to Pratical_2.cpp

--- Pratical_2.cpp
+++ Pratical_2.cpp
@@ -82,17 +82,3 @@ d[i].opc, d[i].oper);
 }
 
 
-macin.dat
-CALC    START   1000
-SUM     MACRO   **
-**      LDA     #5
-**      ADD     #10
-**      STA     2000
-**      MEND    **
-**      LDA     LENGTH
-**      COMP    ZERO
-**      JEQ     LOOP
-**      SUM     **
-LENGTH  WORD    0
-LOOP    SUM     **
-**      END     **
